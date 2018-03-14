3e427240f0a218cc3851bc40ea70c224af15b3bc
Vitaliy Siveniuk <vsyveniu@Vitaliys-Mac-Pro.local>
2018-03-14 20:07:59 +0200

    done

--- scripts/02.sh
+++ scripts/02.sh
@@ -1,10 +1,3 @@
+apt-get update >> /var/log/update_script.log && apt-get -y upgrade >> /var/log/update_script.log
 
-#this script must run under root or sudo(user must be in sudoers list)because cron don't give permisiion for users to change crontab file
-
-echo 'apt-get update >> /var/log/update_script.log && apt-get upgrade >> /var/log/update_script.log' >> ~/updatescript.sh
-
-#command below will add task for root
-
-echo '0 4 * * 1 ~/updatescript.sh' >> /var/spool/cron/crontabs/root && service cron reload
-#echo '0 4 * * 1 ~/updatescript.sh' >> /etc/crontab && service cron reload
-#echo '0 4 * * 1 ~/updatescript.sh' | crontab 
+#echo '00 4 * * 1 ~/02.sh' | crontab
--- scripts/04.sh
+++ scripts/04.sh
@@ -7,14 +7,11 @@ md5sum /etc/crontab | cut -d " " -f 1 > ~/newcrontabsum
 
 res=$(diff ~/crontabsum ~/newcrontabsum)
 
-echo 'suck' > ~/testfile
-
 if [ "$res" ];then
 	md5sum /etc/crontab | cut -d " " -f 1 > ~/crontabsum
 	mail -s "Crontab was changed" $email < ~/crontabsum
-	#mail -s "Crontab was changed" root@localhost.com < ~/crontabsum
 fi
-#use command below to add a task to cron
+
 #echo '0 0 * * * ~/04.sh' >> /etc/crontab && service cron reload
 
 
